e344d2b95778ac1bd408d76295499640a72935c2
jfernandes <jfernandes@wepayments.com.br>
2024-06-03 11:38:33 -0300

    feat: :test_tube: test coroutine parallel

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -12,16 +12,60 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use function Hyperf\Support\now;
+
 class IndexController extends AbstractController
 {
     public function index(): array
     {
-        $user = $this->request->input('user', 'Hyperf');
-        $method = $this->request->getMethod();
+        //TODO:: Test coroutine: https://hyperf.wiki/3.1/#/en/coroutine?id=parallel
 
-        return [
-            'method' => $method,
-            'message' => "Hello {$user}.",
-        ];
+        $parallel = new \Hyperf\Coroutine\Parallel();
+
+        $parallel->add(function () {
+            echo "\n - Entrou para executar a 1\n";
+
+            \Hyperf\Coroutine\Coroutine::sleep(1);
+
+            return now()->format('Y-m-d H:i:s');
+        });
+
+        $parallel->add(function () {
+            echo "\n - Entrou para executar a 2\n";
+
+            \Hyperf\Coroutine\Coroutine::sleep(1);
+
+            return now()->format('Y-m-d H:i:s');
+        });
+
+        $result = $parallel->wait();
+
+//        Result =
+//        [
+//            "2024-06-03 09:27:48",
+//            "2024-06-03 09:27:50"
+//        ]
+
+        //TODO:: Sem coroutine
+//        $result = [];
+//
+//        sleep(1);
+//        $this->addArray($result);
+//
+//        sleep(3);
+//        $this->addArray($result);
+
+//        Result =
+//        [
+//        	"2024-06-03 09:26:16",
+//        	"2024-06-03 09:26:19"
+//        ]
+
+        return $result;
+    }
+
+    function addArray(&$array): void
+    {
+        $array[] = now()->format('Y-m-d H:i:s');
     }
 }
